824f45a5d783c08a3c9dbdf541ef8a8dffbeaa0c
Daniel Levi <daniellevi000@gmail.com>
2024-10-14 10:49:09 -0300

    feat: create heading

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -2,7 +2,7 @@ import { Text, TextProps } from "@design-system/react";
 import { StoryObj, Meta } from "@storybook/react";
 
 export default {
-  title: "Typography/Box",
+  title: "Typography/Text",
   component: Text, 
   args: {
     children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat saepe similique laborum quia iusto voluptatem nihil nemo, ab animi fugit consectetur, deleniti consequuntur aut, eum quidem vel autem maxime non.'
