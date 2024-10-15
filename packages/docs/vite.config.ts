d717df07d2a004aede18970bb300328659a0e5a9
Daniel Levi <daniellevi000@gmail.com>
2024-10-15 15:10:49 -0300

    fix: add subpath storybook production

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Design-System/', // Certifique-se de que este caminho corresponda ao caminho do seu repositório
   optimizeDeps: {
-    exclude: ['DocsRenderer'], 
+    exclude: ['DocsRenderer'], // Excluindo módulos, se necessário
   },
 })
